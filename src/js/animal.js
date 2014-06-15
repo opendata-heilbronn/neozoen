d29105d85418a91b061d1336e52d16a360928b46
Felix Ebert <felix.ebert@if-core.de>
2014-06-15 15:02:50 +0200

    images

--- src/js/animal.js
+++ src/js/animal.js
@@ -1,20 +1,32 @@
 (function (angular, app) {
     'use strict';
 
+    var filterAnimals = function (animals) {
+        var filteredAnimals = [];
+        animals.forEach(function (animal) {
+            if (filteredAnimals.length < 30) {
+                filteredAnimals.push(animal);
+            }
+        });
+        return filteredAnimals;
+    };
+
     angular.module('app').controller('AnimalListCtrl', function ($scope) {
-        $scope.animals = app.animals;
+        $scope.animals = filterAnimals(app.animals);
     });
 
     angular.module('app').controller('AnimalCtrl', function ($scope, $routeParams, $location) {
-        $scope.animals = app.animals;
+        $scope.animals = filterAnimals(app.animals);
 
         var activeAnimal = _.find($scope.animals, function (animal) {
-            return animal.id === $routeParams.animalId;
+            return animal.iD === $routeParams.animalId;
         });
         $scope.activeIndex = _.indexOf($scope.animals, activeAnimal);
 
         $scope.$watch('activeIndex', function () {
-            $location.path('/animals/' + $scope.animals[$scope.activeIndex].id, false);
+            if ($scope.activeIndex && $scope.animals[$scope.activeIndex]) {
+                $location.path('/animals/' + $scope.animals[$scope.activeIndex].iD, false);
+            }
         })
     });
 })(angular, app);

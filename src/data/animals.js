5f02690ae1d939e69d73fb2383fddacaef0477d6
Felix Ebert <felix.ebert@if-core.de>
2014-06-15 11:38:42 +0200

    animal description

--- src/data/animals.js
+++ src/data/animals.js
@@ -2,11 +2,13 @@ app.animals = [
     {
         id: 'aedes_albopictus',
         name: 'Asiatische Tigermücke',
-        img: 'http://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/CDC-Gathany-Aedes-albopictus-1.jpg/2560px-CDC-Gathany-Aedes-albopictus-1.jpg'
+        img: 'http://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/CDC-Gathany-Aedes-albopictus-1.jpg/2560px-CDC-Gathany-Aedes-albopictus-1.jpg',
+        desc: 'Stechmückenart, die als Überträger von Krankheiten wie Chikungunya- und Denguefieber zwischen zwei und zehn Millimeter große, auffällig schwarz-weiß gemusterte Stechmücke. Obwohl die Asiatische Tigermücke in tropischen und subtropischen Regionen beheimatet ist,  passt sie sich erfolgreich an kühlere Regionen an. Sie ist in den feuchtwarmen Tropen ganzjährig aktiv, überwintert aber in gemäßigten Klimaregionen meist im Eistadium.'
     },
     {
         id: 'arion_vulgaris',
         name: 'Spanische Wegschnecke',
-        img: 'http://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Arion_vulgaris_3.jpg/600px-Arion_vulgaris_3.jpg'
+        img: 'http://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Arion_vulgaris_3.jpg/600px-Arion_vulgaris_3.jpg',
+        desc: 'auch Kapuzinerschnecke, Große Wegschnecke oder Lusitanische Wegschnecke genannt, ist eine  8 bis 12 Zentimeter lange Nacktschnecke. Sie gehört        zu der Ordnung der Lungenschnecken (Pulmonata).        In Nutzgärten und landwirtschaftlich genutzten Flächen        stellt die nach Mitteleuropa eingeschleppte Schnecke        eine große Plage dar.        Der Name wurde nach 1956 irrtümlich auf die Art angewendet,    die sich in Mitteleuropa auszubreiten begann und zunächst        in Frankreich und der Schweiz auftrat    Erst 1997 wurde erkannt, dass die Artbestimmung ein    Irrtum war und dass die Schnecke zu einer bis dahin    kaum bekannten Art aus Westfrankreich gehört'
     }
 ]
